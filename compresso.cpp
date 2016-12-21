01935a9aca356d6da95742e8871d7672fde14295
MarshallDyer <mdyer7@mail.csuchico.edu>
2016-12-20 21:46:03 -0800

    made main take in command line arguments with debug output

--- compresso.cpp
+++ compresso.cpp
@@ -6,9 +6,10 @@ Compresso::Compresso() {
 
 }
 
-int main() {
-  Compresso test;
-  cout << "Hello!";
-  
+int main(int argc, char *argv[]) {
+  for (int i = 0; i < argc; i++) {
+    cout << argv[i] << endl;
+  }
+
   return 0;
 }
