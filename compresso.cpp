771fa6a6681af550d1d15e697b9b2e21c4f7aefc
MarshallDyer <mdyer7@mail.csuchico.edu>
2016-12-20 22:01:42 -0800

    Removed debug arg output, added in simple file reading

--- compresso.cpp
+++ compresso.cpp
@@ -1,5 +1,7 @@
 #include "compresso.h"
 #include <iostream>
+#include <vector>
+#include <fstream>
 using namespace std;
 
 Compresso::Compresso() {
@@ -7,9 +9,21 @@ Compresso::Compresso() {
 }
 
 int main(int argc, char *argv[]) {
-  for (int i = 0; i < argc; i++) {
-    cout << argv[i] << endl;
+
+  std::ifstream file(argv[1], std::ios::binary | std::ios::ate);
+  if (file.good()) {
+    std::streamsize size = file.tellg();
+    file.seekg(0, std::ios::beg);
+
+    std::vector<char> buffer(size);
+    if (file.read(buffer.data(), size))
+    {
+        cout << buffer[0];
+    }
+  } else {
+    cout << "\"" << argv[1] << "\" is not a valid file name" << endl;
   }
+
 
   return 0;
 }
